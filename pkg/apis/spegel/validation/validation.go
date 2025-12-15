f3011c61f0f228350b9cea4bd5603e4ec6d296a9
I024114 <dimitar.kostadinov@sap.com>
2025-12-15 12:19:25 +0200

    Use int32 type for ports cfg

--- pkg/apis/spegel/validation/validation.go
+++ pkg/apis/spegel/validation/validation.go
@@ -1,8 +1,10 @@
 package validation
 
 import (
-	"github.com/gardener/gardener-extension-registry-cache/pkg/apis/spegel"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	"github.com/gardener/gardener-extension-registry-cache/pkg/apis/spegel"
 )
 
 // ValidateSpegelConfig validates the passed configuration instance.
@@ -11,12 +13,25 @@ func ValidateSpegelConfig(spegelConfig *spegel.SpegelConfig, fldPath *field.Path
 
 	if spegelConfig.RegistryPort == nil {
 		allErrs = append(allErrs, field.Required(fldPath.Child("registryPort"), "registry port must be provided"))
+	} else {
+		for _, msg := range validation.IsValidPortNum(int(*spegelConfig.RegistryPort)) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("registryPort"), *spegelConfig.RegistryPort, msg))
+		}
 	}
 	if spegelConfig.RouterPort == nil {
 		allErrs = append(allErrs, field.Required(fldPath.Child("routerPort"), "router port must be provided"))
+	} else {
+		for _, msg := range validation.IsValidPortNum(int(*spegelConfig.RouterPort)) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("routerPort"), *spegelConfig.RouterPort, msg))
+		}
 	}
 	if spegelConfig.MetricsPort == nil {
 		allErrs = append(allErrs, field.Required(fldPath.Child("metricsPort"), "metrics port must be provided"))
+	} else {
+		for _, msg := range validation.IsValidPortNum(int(*spegelConfig.MetricsPort)) {
+			allErrs = append(allErrs, field.Invalid(fldPath.Child("metricsPort"), *spegelConfig.MetricsPort, msg))
+		}
 	}
+
 	return allErrs
 }
